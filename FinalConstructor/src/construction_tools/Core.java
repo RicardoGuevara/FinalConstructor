b9847db8c4bc99524ea7640c9d03807a7a2fec8d
RICARDO <RICARDO@DESKTOP-P671MCQ>
2018-06-07 18:59:33 -0500

    core general structure licence headers 0.0.0.0

---
+++ FinalConstructor/src/construction_tools/Core.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 RICARDO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package construction_tools;
+
+/**
+ *Simplifies all the documents creation and management
+ * @author RICARDO
+ */
+public class Core {
+    
+    
+    
+    
+    
+
+//GENERAL PROPIETIES____________________________________________________________
+    
+
+
+//SUB TRHEAD-CLASSES____________________________________________________________    
+   
+    /**
+     *Referent  
+     */
+    class HtmlConstruction implements Runnable
+    {
+
+        @Override
+        public void run() 
+        {
+            
+        }
+    
+    }
+    
+    class JsConstruction implements Runnable
+    {
+
+        @Override
+        public void run() 
+        {
+            
+        }
+    
+    }
+    
+    //in case of implements propietary css
+}
