20fd6695ab70e8351ecdeebc09ef1fce9f3c75b5
RICARDO <RICARDO@DESKTOP-P671MCQ>
2018-06-07 20:37:58 -0500

    core at a half 0.1.0.1

--- FinalConstructor/src/construction_tools/Core.java
+++ FinalConstructor/src/construction_tools/Core.java
@@ -15,19 +15,46 @@
  */
 package construction_tools;
 
-import finalconstructor.Debug;
+import java.io.File;
+import java.util.ArrayList;
 
 /**
  *Simplifies all the documents creation and management
  * @author RICARDO
  */
 public class Core implements finalconstructor.Debug{
-    
+
+    public Core(String title)
+    {
+        Core.setPage_title(title);
+        
+        html_document =     new File(title+".html");
+        js_document =       new File(title+".js");
+        project_document =  new File(title+".hg");
+        
+        components =    new ArrayList<HtmlComponent>();
+        events =        new ArrayList<JsEvent>();
+    }
+
+    public static String getPage_title() {
+        return page_title;
+    }
+
+    public static void setPage_title(String page_title) {
+        Core.page_title = page_title;
+    }
+
     
 
 //GENERAL PROPIETIES____________________________________________________________
     
+    public static ArrayList components,
+                            events;
     
+    private static String page_title;
+    private static File  html_document,
+                        js_document,
+                        project_document;
 
 //SUB TRHEAD-CLASSES____________________________________________________________    
    
--- FinalConstructor/src/construction_tools/JsEvent.java
+++ FinalConstructor/src/construction_tools/JsEvent.java
@@ -13,12 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package construction_tools.models;
+package construction_tools;
 
 /**
  *
  * @author RICARDO
  */
-public class HtmlComponent implements finalconstructor.Debug{
+public class JsEvent implements finalconstructor.Debug{
     
 }
